c9555ca2b7704c558488dffb2dda15b98b6a297b
Dishi Gautam <mail2dishig@gmail.com>
2025-08-31 19:25:45 +0530

    debug

--- server/routes/salesforceRoutes.js
+++ server/routes/salesforceRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import SalesforceService from "../services/salesforceService.js";
+import SalesforceService from "../services/salesforceServices.js";
 import SalesforceToken from "../models/salesforceTokenModel.js";
-import { getUserId } from "../middlewares/auth.js";
+import { getUserId } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
